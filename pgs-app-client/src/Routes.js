7c55a82d0c31406cd81fccb047fcece936c88d65
Kunal Jain <kunal.jain@bizreach.co.jp>
2019-11-14 18:44:35 +0900

    404 routes

--- pgs-app-client/src/Routes.js
+++ pgs-app-client/src/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
+      <Route component={NotFound} />  //undefined routes go to NotFound (default in switch)
     </Switch>
   );
 }
