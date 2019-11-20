09b62dbd1bd3410a556652e097fbd6fcaa8d80e3
Kunal Jain <kunal.jain@bizreach.co.jp>
2019-11-20 11:21:54 +0900

    Added node_modules to gitignore

--- pgs-app-client/src/containers/Login.js
+++ pgs-app-client/src/containers/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
 
 class Login extends React.Component {
   constructor() {
@@ -15,12 +17,13 @@ class Login extends React.Component {
     this.dismissError = this.dismissError.bind(this);
   }
 
+
   dismissError() {
     this.setState({ error: '' });
   }
 
-  handleSubmit(evt) {
-    evt.preventDefault();
+  handleSubmit(event) {
+    event.preventDefault();
 
     if (!this.state.trainerID) {
       return this.setState({ error: 'Trainer ID is required' });
@@ -33,36 +36,55 @@ class Login extends React.Component {
     return this.setState({ error: '' });
   }
 
-  handleUserChange(evt) {
+  handleUserChange(event) {
     this.setState({
-      trainerID: evt.target.value,
+      trainerID: event.target.value,
     });
   };
 
-  handlePassChange(evt) {
+  handlePassChange(event) {
     this.setState({
-      password: evt.target.value,
+      password: event.target.value,
     });
   }
 
   render(){
 
     return(
-      <form onSubmit={this.handleSubmit}>
-        {
-          this.state.error &&
-          <h3 data-test="error" onClick={this.dismissError}>
-            <button onClick={this.dismissError}>✖</button>
-            {this.state.error}
-          </h3>
-        }
-        <label>Trainer ID</label>
-        <input type="text" data-test="trainerID" value={this.state.trainerID} onChange={this.handleUserChange} />
-        <label>Password</label>
-        <input type="password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
-
-        <input type="submit" value="Log In" data-test="submit" />
-      </form>
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Trainer ID</ControlLabel>
+            <FormControl
+              autoFocus
+              type="trainerID"
+              data-test="trainerID"
+              value={this.state.trainerID}
+              onChange={this.handleUserChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              autoFocus
+              type="password"
+              data-test="password"
+              value={this.state.password}
+              onChange={this.handlePassChange}
+            />
+          </FormGroup>
+          <Button block type="submit">
+            Login
+          </Button>
+          {
+            this.state.error &&
+            <h3 data-test="error" onClick={this.dismissError}>
+              <button onClick={this.dismissError}>✖</button>
+              {this.state.error}
+            </h3>
+          }
+        </form>
+      </div>
     )
   }
 }
