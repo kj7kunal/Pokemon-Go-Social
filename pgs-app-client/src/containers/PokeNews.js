52e28afccda4a0e51af9eaae6d84987a8293e73b
Kunal Jain <kunal.jain@bizreach.co.jp>
2019-12-04 16:38:27 +0900

    Added post listing frontend

--- pgs-app-client/src/containers/PokeNews.js
+++ pgs-app-client/src/containers/PokeNews.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, FormGroup, FormControl } from "react-bootstrap";
+import { PageHeader, ListGroup, ListGroupItem , Button, FormGroup, FormControl } from "react-bootstrap";
 import {createPost, getAllPosts} from '../util/APIUtils';
 import "./PokeNews.css";
 
@@ -7,14 +7,41 @@ class PokeNews extends React.Component {
   constructor(props) {
     super(props);
 
-    this.file = null;
-
     this.state = {
       isLoading: false,
-      content: {value:''}
+      content: {value:''},
+      posts: {value:[]}
     };
     this.handleContentChange = this.handleContentChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  loadAllPosts() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    this.setState({
+      isLoading: true
+    });
+
+    getAllPosts()
+    .then(response => {
+      this.setState({
+        posts: response,
+        isLoading: false
+      });
+    })
+    .catch(error => {
+      console.log(error.message);
+      this.setState({
+        isLoading: false
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.loadAllPosts();
   }
 
   validateForm() {
@@ -39,39 +66,66 @@ class PokeNews extends React.Component {
 
     createPost(postRequest)
     .then(response => {
+        this.setState({ isLoading: false });
+        this.setState({ content: {value: '' }});
+        this.loadAllPosts();
         this.props.history.push("/pokenews");
     }).catch(error => {
+        this.setState({ isLoading: false });
+        this.setState({ content: {value: '' }});
         console.log(error.message);
     });
-    this.setState({ content: {value: '' }});
-    this.setState({ isLoading: false });
   }
 
+  renderPostList(posts) {
+    return [{}].concat(this.state.posts).map((post,i) =>
+      i!==0
+      ?<ListGroupItem key={post.id}>
+        <h4>{post.createdBy}</h4>
+        <h3>{post.content}</h3>
+        <h5>{"Last Updated On: " + new Date(post.updatedAt)}</h5>
+      </ListGroupItem>
+      :<ListGroupItem key="new">
+        <h5>
+          ***
+        </h5>
+      </ListGroupItem>
+    ).reverse();
+  }
 
   render() {
     return (
-      <div className="CreatePost">
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="content" bsSize="large">
-            <FormControl
-              autoFocus
-              componentClass="textarea"
-              name="content"
-              placeholder="Enter text.."
-              defaultValue=""
-              required={true}
-              onChange={(event) => this.handleContentChange(event)}
-            />
-          </FormGroup>
-          <Button block type="submit"
-            bsStyle="primary"
-            bsSize="large">
-            POST
-          </Button>
-        </form>
+      <div className="PokeNews">
+        <PageHeader>PokeNews</PageHeader>
+        <div className="CreatePost">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="content" bsSize="large">
+              <FormControl
+                autoFocus
+                componentClass="textarea"
+                name="content"
+                placeholder="Enter text.."
+                defaultValue=""
+                required={true}
+                onChange={(event) => this.handleContentChange(event)}
+              />
+            </FormGroup>
+            <Button block type="submit"
+              bsStyle="primary"
+              bsSize="large">
+              POST
+            </Button>
+          </form>
+        </div>
+        <div className="PostList">
+          <ListGroup>
+            {!this.state.isLoading && this.renderPostList(this.state.posts)}
+          </ListGroup>
+        </div>
       </div>
     );
   }
+
 }
 
 
